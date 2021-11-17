2f194fe4946b551a90d5b4924723588c7b8cdebd
Anilkumar Adhikari <anilkumar.adhikari01@gmail.com>
2021-11-16 18:46:15 -0600

    Made code changes to trigger github workflow

--- src/main/java/com/example/cicdspringboot/CicdspringbootApplication.java
+++ src/main/java/com/example/cicdspringboot/CicdspringbootApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CicdspringbootApplication {
-
+    // Added comment to trigger github workflow
     public static void main(String[] args) {
         SpringApplication.run(CicdspringbootApplication.class, args);
     }
