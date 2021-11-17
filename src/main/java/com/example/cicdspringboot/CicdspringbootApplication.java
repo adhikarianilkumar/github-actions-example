0971d154755fa3dcd82e7530107ac0dd81070320
Anilkumar Adhikari <anilkumar.adhikari01@gmail.com>
2021-11-16 19:03:32 -0600

    Added example api

--- src/main/java/com/example/cicdspringboot/CicdspringbootApplication.java
+++ src/main/java/com/example/cicdspringboot/CicdspringbootApplication.java
@@ -2,12 +2,20 @@ package com.example.cicdspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CicdspringbootApplication {
-    // Added comment to trigger github workflow
+
     public static void main(String[] args) {
         SpringApplication.run(CicdspringbootApplication.class, args);
+    }
+
+    @GetMapping("/example")
+    public String sendMeToHtml(){
+        return "It is working!!";
     }
 
 }
